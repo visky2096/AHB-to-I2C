b6f846b56a8102aab9d916648085ffb1d0ca3b14
visky2096 <vk3829@gmail.com>
2019-02-25 10:56:44 -0500

    Added different type of burst sequences

--- ahb_master_improved.v
+++ ahb_master_improved.v
@@ -2,35 +2,14 @@
 
 * File Name : ahb_master_improved.v
 
-* Purpose :
+* Purpose : Verilog HDL Project 
 
 * Creation Date : 20-02-2019
 
 * Last Modified : 
 
-* Created By : 
-
- \              /  |    _____________			
-  \	       /   |   |		|	 |   	 /\        |
-   \          /    |   |		|	 |	/  \	   |
-    \        /     |   |		|	 |     /    \	   |
-     \      /      |   |_____________	|________|    /______\ 	   |
-      \    /       |		     |	|	 |   /        \	   |
-       \  /        |		     |	|	 |  /	       \   |
-        \/	   |	_____________|	|     	 | /            \  |_____________
-  
-					
-					
-                                                ________
-|    /   |     |   |\      /|        /\        |        |
-|   /	 |     |   | \    / |       /  \       |        |
-|  /     |     |   |  \  /  |      /    \      |        |
-|_/      |     |   |   \/   |     /      \     |________|
-| \      |     |   |        |    /________\    |     \
-|  \     |     |   |        |   /          \   |      \
-|   \    |     |   |        |  /            \  |       \
-|    \	 |_____|   |	    | /              \ |        \
-
+* Created By : VISHAL KUMAR
+                      
 _._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._._.*/
 module ahb_master_improved
 (
@@ -39,7 +18,7 @@ input Hreset,
 input hready,
 input hresp,
 input  [31:0] addr,
-input  [31:0] data,
+input  [0:31] data,
 input  [2:0] size,
 input  [1:0] trans,
 input  [2:0] burst,
@@ -57,11 +36,14 @@ reg [2:0]previous_hburst;
 reg [1:0]previous_htrans;
 reg [31:0]previous_hwdata;
 reg previous_hwrite;
-integer i,k,l,len;
+integer i,len;
+reg [0:31]r;
+reg [0:31]temp_data;
 initial begin 
 i=0;
-l=31;
-k=24;
+//l=31;
+//k=24;
+temp_data=32'hxxxxxxxx;
 previous_haddr=32'bxxxxxxxx;
 previous_hsize=3'bxxx;
 previous_hburst=3'bxxx;
@@ -80,9 +62,21 @@ hsize=3'b000;
 hburst=3'b000;
 hwrite=1'bx;
 end 
-if(Hreset==1 && hreay==1 && hresp==1)
+if(hresp==1)
 begin
-trans=2'b00; 
+ 	htrans=trans; 
+	haddr=addr;
+	hwdata=32'hxxxxxxxx; 
+	hsize=size;
+	hburst=burst;
+	hwrite=1'b1; 
+	previous_htrans=htrans ;
+	previous_haddr=haddr;
+	previous_hwdata=hwdata;
+	previous_hsize=hsize;
+	previous_hburst=hburst;
+	previous_hwrite=hwrite;
+
 end 
 if(Hreset==1 && hready==1 && hresp==0) // for sending the control signals and data 
 begin 
@@ -90,7 +84,7 @@ case (trans)
 2'b00 :                                         //IDLE STATE
 begin 
 	htrans=trans; 
-	haddr=addr;
+	haddr=previous_haddr;
 	hwdata=32'hxxxxxxxx; 
 	hsize=size;
 	hburst=burst;
@@ -121,7 +115,7 @@ begin 	htrans=trans;
 	hwrite=1'b1;
 	previous_htrans=htrans ;
 	previous_haddr=haddr;
-	previous_hwdata=hwdata;
+	previous_hwdata=temp_data;
 	previous_hsize=hsize;
 	previous_hburst=hburst;
 	previous_hwrite=hwrite;
@@ -129,31 +123,36 @@ end
 2'b11 : 				   //SEQUENTIAL 
 begin 	
 	case (burst)
-
+    
 		3'b000: 			//SINGLE BURST 
 			begin 	htrans=trans;
 				haddr=addr;
-				hwdata<=data;
+				temp_data<=data;
+				hwdata<=temp_data;
 				hsize=size;
 				hburst=burst;
 				hwrite=1'b1;
 				previous_htrans=htrans;
 				previous_haddr=haddr;
-				previous_hwdata=hwdata;
+				previous_hwdata=temp_data;
 				previous_hsize=hsize;
 				previous_hburst=hburst;
 				previous_hwrite=hwrite;
 			end 
 		3'b001:                // Increment Burst of 4 beats 
 			begin 	
+				r=32'h00000000;
 				htrans=trans; 
-				for(i=0;i<8;i=i+1)
+				temp_addr=addr;
+				r=r^data;
+				for(i=0;i<4;i=i+1)
 				begin 
-				haddr=addr+4;
-				temp_data<={data[l:k]};
+				haddr=temp_addr;
+				#3;
+				temp_addr=temp_addr+4;
+				temp_data<=r[0:7];
 				hwdata<=temp_data;
-				l=k-1;
-				k=l-7;
+				r=r>>8;
 				end 
 				hburst=burst;
 				hsize=size;
@@ -170,18 +169,22 @@ begin
 		3'b101: 		    // Wraping Burst of 4 beats 
 			
 			begin 	
+				r=32'h00000000;
+				htrans=trans;
 				temp_addr=addr;
+				r=r^data;
 				len=0;
-				for(i=0;i<8;i=i+1) // Number of bursts = 8 
+				for(i=0;i<4;i=i+1) // Number of bursts = 4 
 				begin 
 				if(len==16)begin len=0;haddr=temp_addr; end 
 				else 
-				begin 
-				haddr=addr+4;
-				temp_data<={data[l:k]};
+				begin
+				haddr=temp_addr; 
+				#3;
+				temp_addr=temp_addr+4;
+				temp_data<=r[0:7];
 				hwdata<=temp_data;
-				l=k-1;
-				k=l-7;
+				r=r>>8;
 				len=len+4;
 				end  
 				end 	
@@ -195,14 +198,138 @@ begin
 				previous_hburst=hburst;
 				previous_hwrite=hwrite;
 
+			end 
+
+			3'b001:                // Increment Burst of 8 beats 
+			begin 	
+				r=32'h00000000;
+				htrans=trans; 
+				temp_addr=addr;
+				r=r^data;
+				for(i=0;i<4;i=i+1)
+				begin 
+				haddr=temp_addr;
+				#3;
+				temp_addr=temp_addr+8;
+				temp_data<=r[0:7];
+				hwdata<=temp_data;
+				r=r>>8;
+				end 
+				hburst=burst;
+				hsize=size;
+				hwrite=1'b1;
+				previous_htrans=htrans ;
+				previous_haddr=haddr;
+				previous_hwdata=temp_data;
+				previous_hsize=hsize;
+				previous_hburst=hburst;
+				previous_hwrite=hwrite;
+
+					
+			end 
+		       
+			3'b101: 		    // Wraping Burst of 8 beats 
+			
+			begin 	
+				r=32'h00000000;
+				htrans=trans;
+				temp_addr=addr;
+				r=r^data;
+				len=0;
+				for(i=0;i<4;i=i+1) // Number of bursts = 4 
+				begin 
+				if(len==32)begin len=0;haddr=temp_addr; end 
+				else 
+				begin
+				haddr=temp_addr; 
+				#3;
+				temp_addr=temp_addr+8;
+				temp_data<=r[0:7];
+				hwdata<=temp_data;
+				r=r>>8;
+				len=len+8;
+				end  
+				end 	
+				hburst=burst;
+				hsize=size;
+				hwrite=1'b1;
+				previous_htrans=htrans ;
+				previous_haddr=haddr;
+				previous_hwdata=temp_data;
+				previous_hsize=hsize;
+				previous_hburst=hburst;
+				previous_hwrite=hwrite;
+
 			end 	
+			3'b001:                // Increment Burst of 16 beats 
+			begin 	
+				r=32'h00000000;
+				htrans=trans; 
+				temp_addr=addr;
+				r=r^data;
+				for(i=0;i<4;i=i+1)
+				begin 
+				haddr=temp_addr;
+				#3;
+				temp_addr=temp_addr+16;
+				temp_data<=r[0:7];
+				hwdata<=temp_data;
+				r=r>>8;
+				end 
+				hburst=burst;
+				hsize=size;
+				hwrite=1'b1;
+				previous_htrans=htrans ;
+				previous_haddr=haddr;
+				previous_hwdata=temp_data;
+				previous_hsize=hsize;
+				previous_hburst=hburst;
+				previous_hwrite=hwrite;
+
+					
+			end 
+			3'b101: 		    // Wraping Burst of 16 beats 
+			
+			begin 	
+				r=32'h00000000;
+				htrans=trans;
+				temp_addr=addr;
+				r=r^data;
+				len=0;
+				for(i=0;i<4;i=i+1) // Number of bursts = 4 
+				begin 
+				if(len==64)begin len=0;haddr=temp_addr; end 
+				else 
+				begin
+				haddr=temp_addr; 
+				#3;
+				temp_addr=temp_addr+16;
+				temp_data<=r[0:7];
+				hwdata<=temp_data;
+				r=r>>8;
+				len=len+16;
+				end  
+				end 	
+				hburst=burst;
+				hsize=size;
+				hwrite=1'b1;
+				previous_htrans=htrans ;
+				previous_haddr=haddr;
+				previous_hwdata=temp_data;
+				previous_hsize=hsize;
+				previous_hburst=hburst;
+				previous_hwrite=hwrite;
+
+			end 	
+
+	
 	endcase
 end 		
 		
 
 endcase 
 end
-if(Hreset==1 && hready==0)
+if(Hreset==1 && hready==0 && hresp==0)
 begin
 htrans=previous_htrans;
 haddr=previous_haddr;
